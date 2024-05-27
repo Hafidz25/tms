f35751f04751bee955279716f92ff9834ccd5e7c
Hafidz25 <hafidzalif80@gmail.com>
2024-05-27 11:20:39 +0700

    feat(auth): menambahkan authorization

--- middleware.ts
+++ middleware.ts
@@ -3,14 +3,40 @@ import type { NextRequest } from "next/server";
 import { withAuth } from "next-auth/middleware";
 import { redirect } from "next/navigation";
 
+// export default withAuth(
+//   // `withAuth` augments your `Request` with the user's token.
+//   function middleware(req) {
+//     // console.log(req.nextauth.token);
+//   },
+//   {
+//     callbacks: {
+//       authorized: ({ token }) => !!token,
+//     },
+//   }
+// );
+
 export default withAuth(
   // `withAuth` augments your `Request` with the user's token.
   function middleware(req) {
-    // console.log(req.nextauth.token);
+    console.log(req.nextauth);
+    if (
+      req.nextUrl.pathname === "/dashboard/users" &&
+      req.nextauth.token?.role !== "Admin"
+    ) {
+      return new NextResponse("You are not authorized!");
+    } else if (
+      req.nextUrl.pathname === "/dashboard/briefs/create" &&
+      req.nextauth.token?.role !== "Customer Service"
+    ) {
+      return new NextResponse("You are not authorized!");
+    }
   },
   {
     callbacks: {
-      authorized: ({ token }) => !!token,
+      authorized: (params) => {
+        let { token } = params;
+        return !!token;
+      },
     },
   }
 );
