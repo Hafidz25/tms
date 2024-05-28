e0800455df32be69f37a7a701af3a93f7bcc8b74
Gure-san <azrarafani@gmail.com>
2024-05-28 14:26:34 +0700

    refactor(components): chips
    - memperbaiki struktur state
    - menutup chips menu jika kosong

--- components/ui/chips.tsx
+++ components/ui/chips.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import * as React from "react";
+import React, { useState, useRef, useCallback, SetStateAction } from "react";
 import { X } from "lucide-react";
 
 import { Badge } from "@/components/ui/badge";
@@ -11,33 +11,52 @@ import {
   CommandList,
 } from "@/components/ui/command";
 import { Command as CommandPrimitive } from "cmdk";
-import { userList } from "@/app/constants/userList";
 
-export function Chips({ disable = false }) {
-  const inputRef = React.useRef<HTMLInputElement>(null);
-  const [open, setOpen] = React.useState(false);
-  const [selected, setSelected] = React.useState([
-    userList.filter((user) => user.role === "Team Member")[0],
-  ]);
-  const [inputValue, setInputValue] = React.useState("");
+interface ChipItemProps {
+  id: string;
+  text: string;
+  [key: string]: any;
+}
 
-  const handleUnselect = React.useCallback((framework: any) => {
-    setSelected((prev) => prev.filter((s) => s.name !== framework.name));
-  }, []);
+interface ChipsProps {
+  chipItems: ChipItemProps[];
+  /** digunakan untuk meneruskan nilai yang dipilih */
+  onChange?: (value: ChipItemProps[]) => void;
+  disable?: boolean;
+}
 
-  const handleKeyDown = React.useCallback(
+function Chips({ disable = false, chipItems, onChange }: ChipsProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [open, setOpen] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [chips, setChips] = useState<{
+    selected: ChipItemProps[] | null;
+    list: ChipItemProps[];
+  }>({
+    selected: null,
+    list: [...chipItems],
+  });
+
+  const handleUnselect = (chipId: string) => {
+    if (!chips.selected) return;
+
+    setChips((prevChips) => {
+      return {
+        //@ts-ignore
+        selected: prevChips.selected.filter((c) => c.id !== chipId),
+        list: [
+          ...prevChips.list,
+          chipItems.filter((ci) => ci.id === chipId)[0],
+        ],
+      };
+    });
+  };
+
+  const handleKeyDown = useCallback(
     (e: React.KeyboardEvent<HTMLDivElement>) => {
       const input = inputRef.current;
       if (input) {
-        if (e.key === "Delete" || e.key === "Backspace") {
-          if (input.name === "") {
-            setSelected((prev) => {
-              const newSelected = [...prev];
-              newSelected.pop();
-              return newSelected;
-            });
-          }
-        }
         // This is not a default behaviour of the <input /> field
         if (e.key === "Escape") {
           input.blur();
@@ -47,9 +66,29 @@ export function Chips({ disable = false }) {
     []
   );
 
-  const selectables = userList.filter(
-    (framework) => !selected.includes(framework)
-  );
+  const handleSelect = (nextValue: ChipItemProps) => {
+    setInputValue("");
+    let nextSelected;
+
+    setChips((c) => {
+
+      if (!c.selected) {
+        nextSelected = [nextValue];
+      } else {
+        nextSelected = [...c.selected, nextValue];
+      }
+
+      return {
+        selected: nextSelected,
+        list: [...c.list.filter((ci) => ci.id !== nextValue.id)],
+      };
+    });
+
+    // @ts-ignore
+    if (onChange) onChange(nextSelected);
+  };
+
+  const shouldOpen = open && !!chips.list.length;
 
   return (
     <Command
@@ -57,29 +96,32 @@ export function Chips({ disable = false }) {
       className="overflow-visible bg-transparent"
     >
       <div className="group border border-input px-3 py-2 text-sm  rounded-md">
-        <div className="flex gap-1 flex-wrap">
-          {selected.map((framework) => {
-            return (
-              <Badge key={framework.id} variant="secondary">
-                {framework.name}
-                <button
-                  className="ml-1 ring-offset-background rounded-full outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
-                  onKeyDown={(e) => {
-                    if (e.key === "Enter") {
-                      handleUnselect(framework);
-                    }
-                  }}
-                  onMouseDown={(e) => {
-                    e.preventDefault();
-                    e.stopPropagation();
-                  }}
-                  onClick={() => handleUnselect(framework)}
-                >
-                  <X className="h-3 w-3 text-muted-foreground hover:text-foreground" />
-                </button>
-              </Badge>
-            );
-          })}
+        <div className="flex gap-1.5 flex-wrap">
+          {/* Need help here */}
+          {chips.selected &&
+            chips.selected.map((chip) => {
+              return (
+                <Badge key={chip.id} variant="secondary">
+                  {chip.text}
+                  <button
+                    className="ml-1 ring-offset-background rounded-full outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2"
+                    onKeyDown={(e) => {
+                      if (e.key === "Enter") {
+                        handleUnselect(chip.id);
+                      }
+                    }}
+                    onMouseDown={(e) => {
+                      e.preventDefault();
+                      e.stopPropagation();
+                    }}
+                    onClick={() => handleUnselect(chip.id)}
+                  >
+                    <X className="h-3 w-3 text-muted-foreground hover:text-foreground" />
+                  </button>
+                </Badge>
+              );
+            })}
+
           {/* Avoid having the "Search" Icon */}
           <CommandPrimitive.Input
             disabled={disable}
@@ -93,36 +135,46 @@ export function Chips({ disable = false }) {
           />
         </div>
       </div>
-      <div className="relative">
-        {open && selectables.length > 0 ? (
-          <div className="absolute w-full z-10 top-2 rounded-md border bg-popover text-popover-foreground shadow-md outline-none animate-in">
-            <CommandGroup className="bg-white h-full overflow-auto">
+
+      {shouldOpen && (
+        <div className="relative">
+          <div className="absolute w-full z-[60] top-2 rounded-md border bg-popover text-popover-foreground shadow-md outline-none animate-in">
+            <CommandGroup className="bg-white h-full max-h-[250px] overflow-auto">
               <CommandList>
-                {selectables
-                  .filter((user) => user.role === "Team Member")
-                  .map((framework) => {
-                    return (
-                      <CommandItem
-                        key={framework.id}
-                        onMouseDown={(e) => {
-                          e.preventDefault();
-                          e.stopPropagation();
-                        }}
-                        onSelect={(value) => {
-                          setInputValue("");
-                          setSelected((prev) => [...prev, framework]);
-                        }}
-                        className={"cursor-pointer"}
-                      >
-                        {framework.name}
-                      </CommandItem>
-                    );
-                  })}
+                {chips.list.map((chip) => {
+                  const chipValueSeparator = "-";
+                  const textValue = chip.text.split(" ").join("_");
+
+                  return (
+                    <CommandItem
+                      key={chip.id}
+                      value={`${textValue}${chipValueSeparator}${chip.id}`}
+                      className={"cursor-pointer"}
+                      onSelect={(value) => {
+                        const [text, id] = value.split(chipValueSeparator);
+                        const nextValue = {
+                          id,
+                          text: text.split("_").join(" "),
+                        };
+
+                        handleSelect(nextValue);
+                      }}
+                      onMouseDown={(e) => {
+                        e.preventDefault();
+                        e.stopPropagation();
+                      }}
+                    >
+                      {chip.text}
+                    </CommandItem>
+                  );
+                })}
               </CommandList>
             </CommandGroup>
           </div>
-        ) : null}
-      </div>
+        </div>
+      )}
     </Command>
   );
 }
+
+export { Chips };
