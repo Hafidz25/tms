d586abbc5780a3abda8c87de4689c123b3e397cc
Gure-san <azrarafani@gmail.com>
2024-06-03 10:54:08 +0700

    feat(components): skeleton dashboard panel layouts

--- components/layouts/dashboard-panel.tsx
+++ components/layouts/dashboard-panel.tsx
@@ -1,4 +1,13 @@
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+
+export function LoadDashboardPanel() {
+  return (
+    <div className="animate-pulse w-full h-96 p-10 flex flex-col gap-y-6 bg-slate-100 rounded-md">
+      <div className="bg-slate-200 h-8 w-full rounded-md"></div>
+      <div className="bg-slate-200 h-8 w-full rounded-md"></div>
+    </div>
+  )
+}
 
 export function DashboardPanel({ children, className, ...props }: React.HtmlHTMLAttributes<HTMLDivElement>) {
   return (
