f85778653a9dc49640a1883312bc4de4405da899
Gure-san <azrarafani@gmail.com>
2024-06-03 10:09:04 +0700

    feat(types): briefs

---
+++ types/briefs.d.ts
@@ -0,0 +1,29 @@
+import type { DateRange } from "react-day-picker";
+import type { User } from "./user";
+
+export type BriefsStatus = 
+  'Assigned' |
+  'In Review' |
+  'Waiting Client Feedback' | 
+  'Correction' | 
+  'Done'
+;
+
+export interface Brief {
+  id: string;
+  title: string;
+
+  /**
+   * Type `content` perlu refactor.
+   * Type ini menyimpan data json yang diserialisasi.
+   * Gunakan teknik Type-Safe JSON Serialization.
+   * @see {@link https://hackernoon.com/mastering-type-safe-json-serialization-in-typescript}
+   * */
+  content: string;
+
+  status: BriefsStatus;
+  assign: User[];
+  createdAt: Date;
+  updatedAt: Date;
+  deadline?: DateRange | undefined;
+}
