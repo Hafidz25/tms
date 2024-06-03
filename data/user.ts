6b8787bad78fe757344a84253c058a4df36666f4
Gure-san <azrarafani@gmail.com>
2024-06-03 10:09:32 +0700

    refactor(types): user
    - menandakan `userList` sebagai `depracated`.
    - menerapkan type `CreateSeederProps` pada util `createUser`

--- data/user.ts
+++ data/user.ts
@@ -2,15 +2,17 @@ import { random } from "lodash-es";
 import { fakerID_ID as faker } from "@faker-js/faker";
 import { Roles, User } from "@/types/user";
 import { nanoid } from "nanoid";
+import { CreateSeederProps } from "@/types/seeder";
 
-interface CreateUserProps {
-  amount?: number;
-  role?: Roles | undefined;
-}
+const USER_ROLE: Roles[] = ["Admin", "Customer Service", "Team Member"];
+export const SUPER_ACCOUNT: User = {
+  id: nanoid(4),
+  name: "Abaz",
+  email: "abaz@gmail.com",
+  role: "Admin",
+};
 
-const userRole: Roles[] = ["Admin", "Customer Service", "Team Member"];
-
-function createUser({ amount = 1, role }: CreateUserProps): User[] {
+export function createUser({ amount = 1 }: CreateSeederProps): User[] {
   const result: User[] = [];
 
   for (let x = 0; x < amount; x++) {
@@ -20,13 +22,16 @@ function createUser({ amount = 1, role }: CreateUserProps): User[] {
       id: nanoid(4),
       name: `${firstName} ${lastName}`,
       email: faker.internet.email({ firstName }),
-      role: role ? role : userRole[random(1, 2)],
+      role: USER_ROLE[random(1, 2)],
     });
   }
 
   return result;
 }
 
+/**
+ * @deprecated gunakan util `createUser` daripada variable ini! 
+ * */
 export const userList: User[] = [
   ...createUser({ amount: 9 }),
   {
@@ -36,4 +41,3 @@ export const userList: User[] = [
     role: "Admin",
   },
 ];
-
