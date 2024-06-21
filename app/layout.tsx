f5aece1ab544bd017aec0cbea65cfd4aedbb8adf
Hafidz25 <hafidzalif80@gmail.com>
2024-06-21 10:16:04 +0700

    feat(toast): menambahkan warna toast event

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
         >
           {children}
         </TooltipProvider>
-        <Toaster />
+        <Toaster richColors />
       </body>
     </html>
   );
