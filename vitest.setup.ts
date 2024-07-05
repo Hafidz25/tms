9a375ff554528e8154b4dfdaff8c5b8c4a754e64
Gure-san <azrarafani@gmail.com>
2024-07-05 08:46:24 +0700

    chore(test): mengubah setup vitest

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,23 @@
 import "@testing-library/jest-dom/vitest";
-import { cleanup } from "@testing-library/react";
-import { afterEach } from "vitest";
+import { cleanup, configure } from "@testing-library/react";
+import { afterEach, vi } from "vitest";
+
+// window.PointerEvent = class PointerEvent extends Event {};
+window.HTMLElement.prototype.scrollIntoView = vi.fn();
+window.HTMLElement.prototype.hasPointerCapture = vi.fn();
+window.HTMLElement.prototype.releasePointerCapture = vi.fn();
+
+const ResizeObserverMock = vi.fn(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
+
+vi.stubGlobal("ResizeObserver", ResizeObserverMock);
+
+configure({
+  testIdAttribute: 'data-test-id'
+});
 
 afterEach(() => {
   cleanup();
