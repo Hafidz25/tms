e4c0f00a2574a03b2d78293180383cea5bb7bf0a
Hafidz25 <hafidzalif80@gmail.com>
2024-07-09 10:25:45 +0700

    fix(dashboard): fix line chart

--- components/custom/BriefChart/index.tsx
+++ components/custom/BriefChart/index.tsx
@@ -218,7 +218,7 @@ const BriefChart = () => {
                     />
                   </TooltipTrigger>
                   <TooltipContent side="right">
-                    <p>Refresh</p>
+                    <p>Reset filter</p>
                   </TooltipContent>
                 </Tooltip>
               </TooltipProvider>
