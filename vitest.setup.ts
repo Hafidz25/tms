681da06d6612cbb4bebf49e74077c66a6e686258
Gure-san <azrarafani@gmail.com>
2024-08-05 14:40:25 +0700

    chore(vitest.setup): menggunakan TEST_ID_ATTRIBUTE

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,21 +1,25 @@
 import "@testing-library/jest-dom/vitest";
 import { cleanup, configure } from "@testing-library/react";
 import { afterEach, vi } from "vitest";
+import { TEST_ID_ATTRIBUTE } from "@/lib/test/component";
 
-window.HTMLElement.prototype.scrollIntoView = vi.fn();
-window.HTMLElement.prototype.hasPointerCapture = vi.fn();
-window.HTMLElement.prototype.releasePointerCapture = vi.fn();
+// untuk environment 'jsdom' saja
+if (typeof window !== "undefined") {
+  window.HTMLElement.prototype.scrollIntoView = vi.fn();
+  window.HTMLElement.prototype.hasPointerCapture = vi.fn();
+  window.HTMLElement.prototype.releasePointerCapture = vi.fn();
 
-const ResizeObserverMock = vi.fn(() => ({
-  observe: vi.fn(),
-  unobserve: vi.fn(),
-  disconnect: vi.fn(),
-}));
+  const ResizeObserverMock = vi.fn(() => ({
+    observe: vi.fn(),
+    unobserve: vi.fn(),
+    disconnect: vi.fn(),
+  }));
 
-vi.stubGlobal("ResizeObserver", ResizeObserverMock);
+  vi.stubGlobal("ResizeObserver", ResizeObserverMock);
+}
 
 configure({
-  testIdAttribute: 'data-test-id'
+  testIdAttribute: TEST_ID_ATTRIBUTE,
 });
 
 afterEach(() => {
