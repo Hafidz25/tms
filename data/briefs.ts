9fa4ef8d4d1fd92f308d046e91995045417ef2ec
Gure-san <azrarafani@gmail.com>
2024-08-08 09:53:57 +0700

    fix(data): mengganti logika penambahan data dummy pada `createBriefs`

--- data/briefs.ts
+++ data/briefs.ts
@@ -1,11 +1,10 @@
-import { toDate } from "date-fns";
 import { random } from "lodash-es";
+import { fakerID_ID as faker } from "@faker-js/faker";
 import { Brief, BriefsStatus } from "@/types/brief";
 import { nanoid } from "nanoid";
 import { CreateSeederProps } from "@/types/seeder";
 
-type AssignBrief = Pick<Brief, "assign">;
-type CreateBriefsProps = Partial<AssignBrief> & CreateSeederProps;
+type CreateBriefsProps = Partial<Pick<Brief, "assign">> & CreateSeederProps;
 
 const BRIEF_STATUS: BriefsStatus[] = [
   "Assigned",
@@ -17,28 +16,45 @@ const BRIEF_STATUS: BriefsStatus[] = [
   "Done",
 ];
 
+const YEAR_DELIMITER = 3;
+const DAYS_DELIMITER = 4;
+const REF_DATE = new Date(Date.now());
+
 export function createBriefs({
   amount = 1,
   assign,
 }: CreateBriefsProps): Brief[] {
-  const result: Brief[] = [];
+  return Array.from({ length: amount }).map((item, i) => {
+    const DATE_DETERMINER = random(0, YEAR_DELIMITER);
 
-  for (let x = 0; x < amount; x++) {
-    const TIME = toDate(Date.now());
+    const FROM =
+      DATE_DETERMINER === 0
+        ? faker.date.recent({ days: DAYS_DELIMITER, refDate: REF_DATE })
+        : faker.date.past({ years: YEAR_DELIMITER });
 
-    result.push({
+    const TO = REF_DATE;
+
+    const createdAt = faker.date.between({
+      from: FROM,
+      to: TO,
+    });
+
+    const updatedAt =
+      DATE_DETERMINER === 0
+        ? REF_DATE
+        : faker.date.recent({ days: DAYS_DELIMITER - 1 });
+
+    return {
       id: nanoid(4),
       authorId: nanoid(4),
-      description: `Deskripsi Brief ${x + 1}`,
-      title: `Judul Brief ${x + 1}`,
+      description: `Deskripsi Brief ${i + 1}`,
+      title: `Judul Brief ${i + 1}`,
       status: BRIEF_STATUS[random(0, BRIEF_STATUS.length - 1)],
-      createdAt: TIME,
-      updatedAt: TIME,
+      createdAt,
+      updatedAt,
       assign: !!assign ? assign : [],
       content: "",
       feedback: [],
-    });
-  }
-
-  return result;
+    };
+  });
 }
